ab6c0a86f858a0a899b34c522a96cd182a83cc83
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2024-03-25 21:33:22 +0100

    WI

--- pos/ollama/views.py
+++ pos/ollama/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request
 import ollama
+
 ollama_view = Blueprint('query_model', __name__, url_prefix='/ollama')
+
 
 @ollama_view.route('/get-models')
 def get_models():
@@ -12,10 +14,13 @@ def get_models():
 def create_model():
     base_model_name = request["base_model_name"]
     model_name = request["model_name"]
+    model_description = request["model_description"]
+    
+    # first we need to check if another model with the same name exists
     model_file = f"""
         FROM {base_model_name}
+        SYSTEM {model_description}
     """
-    
     model = ollama.create(model=model_name, 
-                          model_file=model_file)
+                          model_file=model_file)    
     return model
