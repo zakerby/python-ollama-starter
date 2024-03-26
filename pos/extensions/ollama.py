a1c287af130150c8b15ff899995d2444515dd5a6
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2024-03-26 19:12:11 +0100

    Add llama-index as dep, update ollama view

--- pos/extensions/ollama.py
+++ pos/extensions/ollama.py
@@ -1,3 +1,20 @@
 from llama_index.llms.ollama import Ollama
+import ollama
 
-ollama_instance = Ollama(host='localhost', port=11434)
+ollama_instance = Ollama(host='localhost', port=11434, model='gemma:2b')
+
+ollama_client = ollama.Client(host='http://localhost:11434')
+
+
+def list_model():
+    models = ollama_client.list()
+    return models
+
+
+def create_model(base_model_name: str, model_name: str, model_description: str):
+    model_file = f"""
+        FROM {base_model_name}
+        SYSTEM {model_description}
+    """
+    model = ollama_client.create(model=model_name, model_file=model_file)    
+    return model
--- pos/ollama/views.py
+++ pos/ollama/views.py
@@ -1,17 +1,12 @@
 from flask import Blueprint, request
-from llama_index.llms.ollama import Ollama
 
-import ollama
+from pos.extensions import list_model, create_model
 
 ollama_view = Blueprint('ollama_view', __name__, url_prefix='/ollama')
 
-ollama_instance = Ollama(host='localhost', port=11434)
-
-
 @ollama_view.route('/available-models')
 def get_models():
-    models = ollama.list()
-    return models
+    return list_model()
 
 
 @ollama_view.route('/create-model', methods=['POST']) 
@@ -19,12 +14,6 @@ def create_model():
     base_model_name = request["base_model_name"]
     model_name = request["model_name"]
     model_description = request["model_description"]
+    model = create_model(base_model_name, model_name, model_description)
     
-    # first we need to check if another model with the same name exists
-    model_file = f"""
-        FROM {base_model_name}
-        SYSTEM {model_description}
-    """
-    model = ollama.create(model=model_name, 
-                          model_file=model_file)    
     return model
