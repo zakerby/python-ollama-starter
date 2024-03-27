adb7f6d5ed45eac6ee7fefca1ae1c9deefee3219
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2024-03-27 19:00:58 +0100

    Begin migration to LlamaCPP

---
+++ pos/extensions/llm.py
@@ -0,0 +1,24 @@
+from llama_index.llms.llama_cpp import LlamaCPP
+
+
+DEFAULT_MODEL_URL = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q4_0.gguf"
+
+
+def get_llm(model_url=DEFAULT_MODEL_URL) -> LlamaCPP:
+    llm_model = LlamaCPP(
+        # You can pass in the URL to a GGML model to download it automatically
+        model_url=model_url,
+        # optionally, you can set the path to a pre-downloaded model instead of model_url
+        model_path=None,
+        temperature=0.1,
+        max_new_tokens=256,
+        # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+        context_window=3900,
+        # kwargs to pass to __call__()
+        generate_kwargs={},
+        # kwargs to pass to __init__()
+        # set to at least 1 to use GPU
+        model_kwargs={"n_gpu_layers": 1},
+        verbose=True,
+    )
+    return llm_model
---
+++ pos/extensions/rag.py
@@ -0,0 +1,42 @@
+from llama_index.core import QueryBundle
+from llama_index.core.retrievers import BaseRetriever
+from typing import Any, List
+
+
+class VectorDBRetriever(BaseRetriever):
+    """Retriever over a postgres vector store."""
+
+    def __init__(
+        self,
+        vector_store: PGVectorStore,
+        embed_model: Any,
+        query_mode: str = "default",
+        similarity_top_k: int = 2,
+    ) -> None:
+        """Init params."""
+        self._vector_store = vector_store
+        self._embed_model = embed_model
+        self._query_mode = query_mode
+        self._similarity_top_k = similarity_top_k
+        super().__init__()
+
+    def _retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
+        """Retrieve."""
+        query_embedding = embed_model.get_query_embedding(
+            query_bundle.query_str
+        )
+        vector_store_query = VectorStoreQuery(
+            query_embedding=query_embedding,
+            similarity_top_k=self._similarity_top_k,
+            mode=self._query_mode,
+        )
+        query_result = vector_store.query(vector_store_query)
+
+        nodes_with_scores = []
+        for index, node in enumerate(query_result.nodes):
+            score: Optional[float] = None
+            if query_result.similarities is not None:
+                score = query_result.similarities[index]
+            nodes_with_scores.append(NodeWithScore(node=node, score=score))
+
+        return nodes_with_scores
