a294d17ef73e34306f597264791ca3818ecfae9d
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2024-04-07 13:14:14 +0200

    Refactor

---
+++ pos/extensions/db/vector_store.py
@@ -0,0 +1,38 @@
+import psycopg2
+from llama_index.vector_stores.postgres import PGVectorStore
+
+from pos.config import DefaultConfig
+
+db_name = DefaultConfig.PG_VECTOR_DB_NAME
+host = DefaultConfig.PG_VECTOR_HOST
+password = DefaultConfig.PG_VECTOR_PASSWORD
+port = DefaultConfig.PG_VECTOR_PORT
+user = DefaultConfig.PG_VECTOR_USER
+
+
+def init_vector_store():
+    conn = psycopg2.connect(
+        dbname="postgres",
+        host=host,
+        password=password,
+        port=port,
+        user=user,
+    )
+    conn.autocommit = True
+
+    with conn.cursor() as c:
+        c.execute(f"DROP DATABASE IF EXISTS {db_name}")
+        c.execute(f"CREATE DATABASE {db_name}")
+
+
+def get_vector_store() -> PGVectorStore:
+    vector_store = PGVectorStore.from_params(
+        database=db_name,
+        host=host,
+        password=password,
+        port=port,
+        user=user,
+        table_name="llama2_paper",
+        embed_dim=384,  # openai embedding dimension
+    )
+    return vector_store
